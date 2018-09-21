3548be943c8d922e8495a865758d3b6fe58faddf
qqwq <andykwanfai@gmail.com>
2018-09-21 14:23:30 +0000

    add comment

--- 0014. Longest Common Prefix.go
+++ 0014. Longest Common Prefix.go
@@ -1,6 +1,7 @@
 func longestCommonPrefix(strs []string) string {
 	short := shortest(strs)
 
+	//compare each char
 	for i := range short {
 		for j := 0; j < len(strs); j++ {
 			if strs[j][i] != short[i] {
@@ -12,6 +13,7 @@ func longestCommonPrefix(strs []string) string {
 	return short
 }
 
+//find the shortest string
 func shortest(strs []string) string {
 	if len(strs) == 0 {
 		return ""
