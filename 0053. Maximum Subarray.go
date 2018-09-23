4c7efac0dc4bdc6f571dc41b46fc018773a48a6c
qqwq <andykwanfai@gmail.com>
2018-09-23 12:33:20 +0000

    add 0053

---
+++ 0053. Maximum Subarray.go
@@ -0,0 +1,19 @@
+func maxSubArray(nums []int) int {
+	length := len(nums)
+	dp := make([]int, length)
+	dp[0] = nums[0]
+	maximum := dp[0]
+	for i := 1; i < length; i++ {
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		maximum = max(maximum, dp[i])
+	}
+	return maximum
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
